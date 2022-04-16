c7a0272050ebc2368ae7f317771141facb3ac70e
arcsine <tangscn122@gmail.com>
2022-04-17 05:03:05 +0800

    register works now

--- app.py
+++ app.py
@@ -5,6 +5,256 @@ import MySQLdb.cursors
 from logging import FileHandler,WARNING
 import re
 from flask_session import Session
+from datetime import datetime
+
+countrydict = {"AF":"Afghanistan",
+"AX":"Aland Islands",
+"AL":"Albania",
+"DZ":"Algeria",
+"AS":"American Samoa",
+"AD":"Andorra",
+"AO":"Angola",
+"AI":"Anguilla",
+"AQ":"Antarctica",
+"AG":"Antigua and Barbuda",
+"AR":"Argentina",
+"AM":"Armenia",
+"AW":"Aruba",
+"AU":"Australia",
+"AT":"Austria",
+"AZ":"Azerbaijan",
+"BS":"Bahamas",
+"BH":"Bahrain",
+"BD":"Bangladesh",
+"BB":"Barbados",
+"BY":"Belarus",
+"BE":"Belgium",
+"BZ":"Belize",
+"BJ":"Benin",
+"BM":"Bermuda",
+"BT":"Bhutan",
+"BO":"Bolivia, Plurinational State of",
+"BQ":"Bonaire, Sint Eustatius and Saba",
+"BA":"Bosnia and Herzegovina",
+"BW":"Botswana",
+"BV":"Bouvet Island",
+"BR":"Brazil",
+"IO":"British Indian Ocean Territory",
+"BN":"Brunei Darussalam",
+"BG":"Bulgaria",
+"BF":"Burkina Faso",
+"BI":"Burundi",
+"KH":"Cambodia",
+"CM":"Cameroon",
+"CA":"Canada",
+"CV":"Cape Verde",
+"KY":"Cayman Islands",
+"CF":"Central African Republic",
+"TD":"Chad",
+"CL":"Chile",
+"CN":"China",
+"CX":"Christmas Island",
+"CC":"Cocos (Keeling) Islands",
+"CO":"Colombia",
+"KM":"Comoros",
+"CG":"Congo",
+"CD":"Congo, The Democratic Republic of the",
+"CK":"Cook Islands",
+"CR":"Costa Rica",
+"CI":"Côte d'Ivoire",
+"HR":"Croatia",
+"CU":"Cuba",
+"CW":"Curaçao",
+"CY":"Cyprus",
+"CZ":"Czech Republic",
+"DK":"Denmark",
+"DJ":"Djibouti",
+"DM":"Dominica",
+"DO":"Dominican Republic",
+"EC":"Ecuador",
+"EG":"Egypt",
+"SV":"El Salvador",
+"GQ":"Equatorial Guinea",
+"ER":"Eritrea",
+"EE":"Estonia",
+"ET":"Ethiopia",
+"FK":"Falkland Islands (Malvinas)",
+"FO":"Faroe Islands",
+"FJ":"Fiji",
+"FI":"Finland",
+"FR":"France",
+"GF":"French Guiana",
+"PF":"French Polynesia",
+"TF":"French Southern Territories",
+"GA":"Gabon",
+"GM":"Gambia",
+"GE":"Georgia",
+"DE":"Germany",
+"GH":"Ghana",
+"GI":"Gibraltar",
+"GR":"Greece",
+"GL":"Greenland",
+"GD":"Grenada",
+"GP":"Guadeloupe",
+"GU":"Guam",
+"GT":"Guatemala",
+"GG":"Guernsey",
+"GN":"Guinea",
+"GW":"Guinea-Bissau",
+"GY":"Guyana",
+"HT":"Haiti",
+"HM":"Heard Island and McDonald Islands",
+"VA":"Holy See (Vatican City State)",
+"HN":"Honduras",
+"HK":"Hong Kong",
+"HU":"Hungary",
+"IS":"Iceland",
+"IN":"India",
+"ID":"Indonesia",
+"IR":"Iran, Islamic Republic of",
+"IQ":"Iraq",
+"IE":"Ireland",
+"IM":"Isle of Man",
+"IL":"Israel",
+"IT":"Italy",
+"JM":"Jamaica",
+"JP":"Japan",
+"JE":"Jersey",
+"JO":"Jordan",
+"KZ":"Kazakhstan",
+"KE":"Kenya",
+"KI":"Kiribati",
+"KP":"Korea, Democratic People's Republic of",
+"KR":"Korea, Republic of",
+"KW":"Kuwait",
+"KG":"Kyrgyzstan",
+"LA":"Lao People's Democratic Republic",
+"LV":"Latvia",
+"LB":"Lebanon",
+"LS":"Lesotho",
+"LR":"Liberia",
+"LY":"Libya",
+"LI":"Liechtenstein",
+"LT":"Lithuania",
+"LU":"Luxembourg",
+"MO":"Macao",
+"MK":"Macedonia, Republic of",
+"MG":"Madagascar",
+"MW":"Malawi",
+"MY":"Malaysia",
+"MV":"Maldives",
+"ML":"Mali",
+"MT":"Malta",
+"MH":"Marshall Islands",
+"MQ":"Martinique",
+"MR":"Mauritania",
+"MU":"Mauritius",
+"YT":"Mayotte",
+"MX":"Mexico",
+"FM":"Micronesia, Federated States of",
+"MD":"Moldova, Republic of",
+"MC":"Monaco",
+"MN":"Mongolia",
+"ME":"Montenegro",
+"MS":"Montserrat",
+"MA":"Morocco",
+"MZ":"Mozambique",
+"MM":"Myanmar",
+"NA":"Namibia",
+"NR":"Nauru",
+"NP":"Nepal",
+"NL":"Netherlands",
+"NC":"New Caledonia",
+"NZ":"New Zealand",
+"NI":"Nicaragua",
+"NE":"Niger",
+"NG":"Nigeria",
+"NU":"Niue",
+"NF":"Norfolk Island",
+"MP":"Northern Mariana Islands",
+"NO":"Norway",
+"OM":"Oman",
+"PK":"Pakistan",
+"PW":"Palau",
+"PS":"Palestinian Territory, Occupied",
+"PA":"Panama",
+"PG":"Papua New Guinea",
+"PY":"Paraguay",
+"PE":"Peru",
+"PH":"Philippines",
+"PN":"Pitcairn",
+"PL":"Poland",
+"PT":"Portugal",
+"PR":"Puerto Rico",
+"QA":"Qatar",
+"RE":"Réunion",
+"RO":"Romania",
+"RU":"Russian Federation",
+"RW":"Rwanda",
+"BL":"Saint Barthélemy",
+"SH":"Saint Helena, Ascension and Tristan da Cunha",
+"KN":"Saint Kitts and Nevis",
+"LC":"Saint Lucia",
+"MF":"Saint Martin (French part)",
+"PM":"Saint Pierre and Miquelon",
+"VC":"Saint Vincent and the Grenadines",
+"WS":"Samoa",
+"SM":"San Marino",
+"ST":"Sao Tome and Principe",
+"SA":"Saudi Arabia",
+"SN":"Senegal",
+"RS":"Serbia",
+"SC":"Seychelles",
+"SL":"Sierra Leone",
+"SG":"Singapore",
+"SX":"Sint Maarten (Dutch part)",
+"SK":"Slovakia",
+"SI":"Slovenia",
+"SB":"Solomon Islands",
+"SO":"Somalia",
+"ZA":"South Africa",
+"GS":"South Georgia and the South Sandwich Islands",
+"ES":"Spain",
+"LK":"Sri Lanka",
+"SD":"Sudan",
+"SR":"Suriname",
+"SS":"South Sudan",
+"SJ":"Svalbard and Jan Mayen",
+"SZ":"Swaziland",
+"SE":"Sweden",
+"CH":"Switzerland",
+"SY":"Syrian Arab Republic",
+"TW":"Taiwan, Province of China",
+"TJ":"Tajikistan",
+"TZ":"Tanzania, United Republic of",
+"TH":"Thailand",
+"TL":"Timor-Leste",
+"TG":"Togo",
+"TK":"Tokelau",
+"TO":"Tonga",
+"TT":"Trinidad and Tobago",
+"TN":"Tunisia",
+"TR":"Turkey",
+"TM":"Turkmenistan",
+"TC":"Turks and Caicos Islands",
+"TV":"Tuvalu",
+"UG":"Uganda",
+"UA":"Ukraine",
+"AE":"United Arab Emirates",
+"GB":"United Kingdom",
+"US":"United States",
+"UM":"United States Minor Outlying Islands",
+"UY":"Uruguay",
+"UZ":"Uzbekistan",
+"VU":"Vanuatu",
+"VE":"Venezuela, Bolivarian Republic of",
+"VN":"Viet Nam",
+"VG":"Virgin Islands, British",
+"VI":"Virgin Islands, U.S.",
+"WF":"Wallis and Futuna",
+"YE":"Yemen",
+"ZM":"Zambia",
+"ZW":"Zimbabwe"}
 
 app = Flask(__name__)  
 app.config["SESSION_PERMANENT"] = False
@@ -27,7 +277,7 @@ def index():
 
 @app.route("/tournament")
 def tournament():
-    return render_template("index.html")   
+    return render_template("tournament.html")   
 
 @app.route("/song")
 def song():
@@ -37,9 +287,23 @@ def song():
 def user():
     return render_template("user.html")
 
-@app.route("/register")
-def registerhtml():
-    return render_template("register.html")
+@app.route("/register", methods=['GET','POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form:
+        username = request.form['name']
+        password = request.form['password']
+        country = request.form['country']
+        if password != request.form['cfmpassword']:
+            msg = "The passwords do not match!"
+            return render_template('register.html', msg=msg, countries = countrydict)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        temp = 'INSERT INTO user (username, password, country, globalrank, countryrank, ranklastupdated, mainmode) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+        val = (username, password, country, None, None, datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 0)
+        cursor.execute(temp, val)
+        mysql.connection.commit()
+        return redirect(url_for('login'))
+    return render_template("register.html", countries = countrydict)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -77,22 +341,6 @@ def logout():
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-    elif request.method == 'POST':
-        # Form is empty... (no POST data)
-        msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
-    return render_template('register.html', msg=msg)
 
 if __name__ == '__main__':
     app.run(debug=True)
